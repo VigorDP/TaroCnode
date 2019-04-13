566193f566e5ef9d5a496ff83e49a4c3e3a67a00
fengyinchao <1542963276@qq.com>
2019-04-13 19:39:11 +0800

    feat：完善腳手架

--- config/index.js
+++ config/index.js
@@ -16,7 +16,6 @@ const config = {
     'constants': path.resolve(__dirname, '..', 'src/constants'),
     'api': path.resolve(__dirname, '..', 'src/api'),
     'assets': path.resolve(__dirname, '..', 'src/assets'),
-
     'utils': path.resolve(__dirname, '..', 'src/utils'),
   },
   sourceRoot: 'src',
